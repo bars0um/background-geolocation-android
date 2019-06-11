f6907f01cb202d0ccfc8abdc4d81812a4663dad5
Nadim Barsoum <nadim.barsoum@via-eg.com>
2019-06-11 17:25:50 +0200

    further logger reversals

--- src/main/java/com/marianhello/bgloc/provider/RawLocationProvider.java
+++ src/main/java/com/marianhello/bgloc/provider/RawLocationProvider.java
@@ -8,6 +8,7 @@ import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.marianhello.bgloc.Config;
+import com.marianhello.logging.LoggerManager;
 
 /**
  * Created by finch on 7.11.2017.
